9f0eea51762ac759f6b23da510a6b2185471eb6d
unknown <kuldipw00@gmail.com>
2020-11-15 20:56:21 +0530

    fronend

--- server.js
+++ server.js
@@ -5,12 +5,25 @@ const path=require('path')
 const expressLayouts=require("express-ejs-layouts")
 const { dirname } = require('path')
 app.use(express.static('public'))
+app.use(expressLayouts)
+
+app.set('views',path.join(__dirname,'/resources/views'))
+app.set('view engine','ejs')
 app.get('/',(req,res)=>{
     res.render('home')
 })
+app.get('/cart',(req,res)=>{
+    res.render('customers/cart')
+})
+app.get('/login',(req,res)=>{
+    res.render('auth/login')
+})
+app.get('/register',(req,res)=>{
+    res.render('auth/register')
+})
 
-// set template engine
-app.use(expressLayouts)
+
+
 
 
 const port=process.env.PORT || 3000
