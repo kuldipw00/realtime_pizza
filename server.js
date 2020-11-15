4bd332b9509cebb010a0f1585cad160e47371092
unknown <kuldipw00@gmail.com>
2020-11-15 12:10:19 +0530

    second

--- server.js
+++ server.js
@@ -4,6 +4,7 @@ const ejs=require("ejs")
 const path=require('path')
 const expressLayouts=require("express-ejs-layouts")
 const { dirname } = require('path')
+app.use(express.static('public'))
 app.get('/',(req,res)=>{
     res.render('home')
 })
