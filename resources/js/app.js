5756dfe859c1ee84fca848a8e35fb3b73fbc2035
unknown <kuldipw00@gmail.com>
2020-11-18 13:16:44 +0530

    add to cart

--- resources/js/app.js
+++ resources/js/app.js
@@ -1,1 +1,31 @@
-console.log("hello world")
+
+import axios from 'axios'
+import Noty from 'noty'
+const { update } = require("../../app/http/models/menu");
+
+let addToCart=document.querySelectorAll('.add-cart')
+let cartCounter=document.querySelector('#cartCounter')
+
+function updateCart(pizza){
+    axios.post('/update-cart',pizza).then(res=>{
+        
+        cartCounter.innerText=res.data.totalQty
+
+        new Noty({
+            type:'success',
+            timeout:500,
+            text:'Items added to cart',
+            progressBar:false
+        }).show()
+
+
+    })
+}
+addToCart.forEach((btn)=>{
+    btn.addEventListener('click',(e)=>{
+        
+        let pizza=JSON.parse(btn.dataset.pizza);
+        updateCart(pizza)
+      
+    })
+})
--- server.js
+++ server.js
@@ -4,24 +4,53 @@ const ejs=require("ejs")
 const path=require('path')
 const expressLayouts=require("express-ejs-layouts")
 const { dirname } = require('path')
+
+const mongoose=require('mongoose')
+const session = require('express-session')
+const flash=require('express-flash')
+const MongoDbStore =require('connect-mongo')(session)
+//database cneection
+
+const url="mongodb://127.0.0.1:27017/pizzas";
+mongoose.connect(url,{useNewUrlParser:true,useCreateIndex:true,useUnifiedTopology:true,useFindAndModify:true});
+const connection =mongoose.connection;
+require('dotenv').config()
+connection.once('open',()=>{
+    console.log('Databse connected!!')
+}).catch(res=>{
+    console.log('connection failed...')
+})
+
+let mongoStore=new MongoDbStore({
+    mongooseConnection:connection,
+    collection:'session'
+})
+app.use(express.json())
+app.use(session({
+    secret:process.env.COOKIE_SECRET,
+    resave:false,
+    store:mongoStore,
+    saveUninitialized:false,
+    cookie:{maxAge:1000*60*60*24}//24hours
+}))
+
+app.use(flash())
 app.use(express.static('public'))
+//global middleware
+
+app.use((req,res,next)=>{
+    res.locals.session=req.session
+    next()    
+})
 app.use(expressLayouts)
+
+
 
 app.set('views',path.join(__dirname,'/resources/views'))
 app.set('view engine','ejs')
-app.get('/',(req,res)=>{
-    res.render('home')
-})
-app.get('/cart',(req,res)=>{
-    res.render('customers/cart')
-})
-app.get('/login',(req,res)=>{
-    res.render('auth/login')
-})
-app.get('/register',(req,res)=>{
-    res.render('auth/register')
-})
 
+
+require('./routes/web')(app)
 
 
 
